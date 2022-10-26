488d3ab135ca3c777b4565f87e0549418a3ec4a0
Gabriel <jgabriel.orozco@udea.edu.co>
2022-10-25 20:43:28 -0500

    Se crea el timer periodico que dispara las mediciones de los sensores.

--- neptuneProject.c
+++ neptuneProject.c
@@ -1,12 +1,41 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "event_groups.h"
+#include "mpu9250.h"
+#include "hardware/spi.h"
+#include "hardware/adc.h"
+#include "hardware/timer.h"
+
+//Timers del FreeRTOS
+#include "timers.h"
+
+#define measureTIMER_PERIOD pdMS_TO_TICKS(2500)
+
+//Callback del timer cada seg
+void measureTimerCallback(TimerHandle_t xTimer);
 
 
 int main()
 {
     stdio_init_all();
+    TimerHandle_t xMeasureTimer;
+    BaseType_t xMeasureTimerStarted;
 
-    puts("Hello, world!");
+    xMeasureTimer = xTimerCreate("MeasureTimer", measureTIMER_PERIOD, pdTRUE, 0, measureTimerCallback);
+    
 
-    return 0;
+    if(xMeasureTimer != NULL){
+        xMeasureTimerStarted = xTimerStart(xMeasureTimer, 0);
+
+        if(xMeasureTimerStarted == pdPASS){
+            vTaskStartScheduler();
+        }
+    }
+    for(;;);
+}
+
+void measureTimerCallback(TimerHandle_t xTimer){
+    printf("Este mensaje se mostrará cada 2.5 segundos\n");
 }
