68db24dab72af3d1cf701283bdec4d657096dc29
Gabriel <jgabriel.orozco@udea.edu.co>
2022-11-01 12:38:31 -0500

    Se logra lanzar una tarea que espera por un bit de evento.

--- neptuneProject.c
+++ neptuneProject.c
@@ -11,31 +11,58 @@
 //Timers del FreeRTOS
 #include "timers.h"
 
-#define measureTIMER_PERIOD pdMS_TO_TICKS(2500)
+//Grupo de eventos para tomar medidas
+#define measureBIT_0 (1UL << 0UL)
+#define measureBIT_1 (1UL << 1UL)
+#define measureBIT_2 (1UL << 2UL)
+#define measureBIT_3 (1UL << 3UL)
 
-//Callback del timer cada seg
-void measureTimerCallback(TimerHandle_t xTimer);
+//Event group de medida
+EventGroupHandle_t xEventGroup;
 
+//Tareas
+void readIMUTask(void *pvParameters);
+
+void settingTask(void *pvParameters);
 
 int main()
 {
     stdio_init_all();
-    TimerHandle_t xMeasureTimer;
-    BaseType_t xMeasureTimerStarted;
 
-    xMeasureTimer = xTimerCreate("MeasureTimer", measureTIMER_PERIOD, pdTRUE, 0, measureTimerCallback);
+    sleep_ms(5000);
+    printf("Iniciando...\r\n");
+
+    printf("Creando grupos de eventos...\r\n");
+    xEventGroup = xEventGroupCreate();
+
+    printf("Creando tareas...\r\n");
+    xTaskCreate(readIMUTask, "IMUReader", 1000, NULL, 2, NULL);
+    xTaskCreate(settingTask, "BitSetter", 1000, NULL, 1, NULL);
     
+    printf("Iniciando scheduler...\r\n");
+    vTaskStartScheduler();
 
-    if(xMeasureTimer != NULL){
-        xMeasureTimerStarted = xTimerStart(xMeasureTimer, 0);
-
-        if(xMeasureTimerStarted == pdPASS){
-            vTaskStartScheduler();
-        }
-    }
     for(;;);
+    return 0;
 }
 
-void measureTimerCallback(TimerHandle_t xTimer){
-    printf("Este mensaje se mostrará cada 2.5 segundos\n");
+void settingTask(void *pvParamters){
+    const TickType_t xDelay1sec = pdMS_TO_TICKS(1000UL), xDontBlock = 0;
+    while(true){
+        vTaskDelay(xDelay1sec);
+        printf("Seteando bit 0...\r\n");
+        xEventGroupSetBits(xEventGroup, measureBIT_0);
+    }
+}
+
+void readIMUTask(void *pvParameters){
+    EventBits_t xEventGroupValue;
+    const EventBits_t xBitsToWaitFor = measureBIT_0;
+    while(true){
+        xEventGroupValue = xEventGroupWaitBits(xEventGroup, xBitsToWaitFor, pdTRUE, pdTRUE, portMAX_DELAY);
+        printf("Iniciando lectura de IMU...\n");
+        /*
+            FUNCIONES DE LECTURA DE LA IMU
+        */
+    }
 }
